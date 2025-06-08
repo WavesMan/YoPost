5af143007337ba6b96d5be55fa071897df9ef3d6
WavesMan <1690611689@qq.com>
2025-06-08 14:12:23 +0800

    chore(vite): 禁用CSS压缩并保持输出目录不变
    禁用构建时的CSS压缩功能，同时保持web资源输出目录不变

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   build:{
-    outDir: '../cmd/server/web'
+    cssMinify: false,
+    outDir: '../cmd/server/web',
   },
   plugins: [react()],
 })
